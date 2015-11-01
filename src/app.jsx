fd234551a0ef24036476b28833b5c6e0b5cb0034
Brian <bgrober@gmail.com>
2015-11-01 15:58:39 -0800

    refs updated

--- src/app.jsx
+++ src/app.jsx
@@ -2,110 +2,52 @@
 
 var React = require('react');
 var ReactDOM = require('react-dom');
-var _ = require('lodash');
 // tutorial1.js
-var Welcome = React.createClass({
-  getInitialState: function() {
-    console.log('GetInitialState');
-    return {foo : 1};
-  },
-
-  getDefaultProps: function() {
-      console.log('GetDefaultProps');
-      return {bar: 1};
-  },
-
-  update: function() {
-    console.log('Updating State');
-    var count = this.state.foo;
-    count++;
-    this.setState({foo: count});
-  },
-
-  render: function() {
-    console.log('Render');
-    return (<div>
-      This.state.foo: {this.state.foo} <br />
-      This.state.bar: {this.props.bar}
-      <br/>
-      <hr/>
-      <button className="btn btn-success"
-        onClick={this.update}>
-        Update State
-      </button>
-    </div>);
-  },
-
-  componentWillMount: function() {
-    console.log('ComponentWillMount');
-  },
-
-  componentDidMount: function() {
-    console.log('ComponentDidMount');
-  },
-
-  shouldComponentUpdate: function(nextProps, nextState) {
-    console.log('ShouldComponentUpdate');
-    console.log(nextProps.bar, nextState.foo);
-    return nextProps.bar === nextState.foo ? false : true;
-    return true;
-  },
-
-  componentWillReceiveProps: function(nextProps) {
-    console.log('ComponentWillRecieveProps');
-  },
-
-  componentWillUpdate: function() {
-    console.log('ComponentWillUpdate');
-  },
-
-  componentDidUpdate: function() {
-    console.log('ComponentDidUpdate');
-  },
-
-  componentWillUnmount: function() {
-    console.log('componentWillUnmount');
-  }
-});
-
 var App = React.createClass({
   getInitialState: function() {
-      return {id: 1};
+      return {
+        red: 0,
+        green: 0,
+        blue: 0
+      };
   },
 
   update: function(e) {
-     console.log('Updating Props');
-     console.log(e);
-     var count = this.state.id;
-     count++;
-     this.setState({id: count});
+     this.setState({
+       red: this.refs.red.getDOMNode().value,
+       green: this.refs.green.getDOMNode().value,
+       blue: this.refs.blue.getDOMNode().value
+     });
   },
 
   render: function() {
     return (
       <div>
-      <hr/>
-      <Welcome bar={this.state.id} />
-      <hr />
-      <button type="button" className="btn btn-primary"
-        onClick={this.update} onTouchStart={this.update}>
-        Update Props
-      </button>
-  </div>
+        {this.state.txt}
+        <hr/>
+        <Widget ref='red' txt={this.state.txt} update={this.update}/>
+        <label>{this.state.red}</label><br/>
+        <Widget ref='green' txt={this.state.txt} update={this.update}/>
+        <label>{this.state.green}</label><br/>
+        <Widget ref='blue' txt={this.state.txt} update={this.update}/>
+        <label>{this.state.blue}</label>
+      </div>
     )
   }
 });
+
+var Widget = React.createClass({
+  render: function() {
+    return (
+      <input type='range' min='0' max='255' onChange={this.props.update}/>
+    )
+  }
+
+
+
+})
 
 ReactDOM.render(
   <App />,
   document.getElementById('app')
 );
-
-var unmountBtn = document.getElementById('unmount');
-unmountBtn.addEventListener('click', unmount);
-
-function unmount() {
-  console.log('Unmounting');
-  ReactDOM.unmountComponentAtNode(document.getElementById('app'));
-  unmountBtn.remove();
-}
