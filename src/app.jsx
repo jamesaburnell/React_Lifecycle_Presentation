b3629909d1340aedd7f7730cba5e1ba9ce73eb2a
Brian <bgrober@gmail.com>
2015-11-01 15:41:23 -0800

    simple example

--- src/app.jsx
+++ src/app.jsx
@@ -2,62 +2,110 @@
 
 var React = require('react');
 var ReactDOM = require('react-dom');
-
+var _ = require('lodash');
 // tutorial1.js
-var Box = React.createClass({
+var Welcome = React.createClass({
+  getInitialState: function() {
+    console.log('GetInitialState');
+    return {foo : 1};
+  },
 
+  getDefaultProps: function() {
+      console.log('GetDefaultProps');
+      return {bar: 1};
+  },
+
+  update: function() {
+    console.log('Updating State');
+    var count = this.state.foo;
+    count++;
+    this.setState({foo: count});
+  },
 
   render: function() {
-    return (
-      <button onClick={this.props.handleClick} className="box" style={{height:'100px',width:'100px'}}>
-        {this.props.char}
+    console.log('Render');
+    return (<div>
+      This.state.foo: {this.state.foo} <br />
+      This.state.bar: {this.props.bar}
+      <br/>
+      <hr/>
+      <button className="btn btn-success"
+        onClick={this.update}>
+        Update State
       </button>
-    );
+    </div>);
+  },
+
+  componentWillMount: function() {
+    console.log('ComponentWillMount');
+  },
+
+  componentDidMount: function() {
+    console.log('ComponentDidMount');
+  },
+
+  shouldComponentUpdate: function(nextProps, nextState) {
+    console.log('ShouldComponentUpdate');
+    console.log(nextProps.bar, nextState.foo);
+    return nextProps.bar === nextState.foo ? false : true;
+    return true;
+  },
+
+  componentWillReceiveProps: function(nextProps) {
+    console.log('ComponentWillRecieveProps');
+  },
+
+  componentWillUpdate: function() {
+    console.log('ComponentWillUpdate');
+  },
+
+  componentDidUpdate: function() {
+    console.log('ComponentDidUpdate');
+  },
+
+  componentWillUnmount: function() {
+    console.log('componentWillUnmount');
   }
 });
-var Row = React.createClass({
 
-  render: function() {
-    return (
-      <div className="row">
-        <Box key="0"  handleClick={this.props.handleClick} char={this.props.data[0]}/>
-        <Box key='1'  handleClick={this.props.handleClick} char={this.props.data[1]}/>
-        <Box key='2'  handleClick={this.props.handleClick} char={this.props.data[2]}/>
-      </div>
-
-    )
-  }
-})
-
-var Board = React.createClass({
+var App = React.createClass({
   getInitialState: function() {
-    return {winCheck: [
-       ['-','-','-'],
-       ['-','-','-'],
-       ['-','-','-']
-     ]
-    }
+      return {id: 1};
   },
-  tick: function (arg,param){
-    // debugger;
-    var rowClicked = param.charAt(4)
-    var boxClicked = param.charAt(7)
-    if(this.state.winCheck[rowClicked][boxClicked] === 'X') this.state.winCheck[rowClicked][boxClicked] = 'O'
-    else this.state.winCheck[rowClicked][boxClicked] = 'X'
-    this.setState ({winCheck: this.state.winCheck})
+
+  update: function(e) {
+     console.log('Updating Props');
+     console.log(e);
+     var count = this.state.id;
+     count++;
+     this.setState({id: count});
   },
 
   render: function() {
     return (
-      <div className="board">
-        <Row key='0' handleClick={this.tick} data={this.state.winCheck[0]}/>
-        <Row key='1' handleClick={this.tick} data={this.state.winCheck[1]}/>
-        <Row key='2' handleClick={this.tick} data={this.state.winCheck[2]}/>
-      </div>
+      <div>
+      <hr/>
+      <Welcome bar={this.state.id} />
+      <hr />
+      <button type="button" className="btn btn-primary"
+        onClick={this.update} onTouchStart={this.update}>
+        Update Props
+      </button>
+  </div>
     )
   }
-})
+});
+
 ReactDOM.render(
-  <Board />,
-  document.body
+  <App />,
+  document.getElementById('app')
 );
+
+var unmountBtn = document.getElementById('unmount');
+unmountBtn.addEventListener('click', unmount);
+
+function unmount() {
+  console.log('Unmounting');
+  ReactDOM.unmountComponentAtNode(document.getElementById('app'));
+  unmountBtn.remove();
+}
